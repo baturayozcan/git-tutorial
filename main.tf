cb48b8520cbbb8c680077f14c29e8db563397fc7
Baturay Ozcan <baturayozcan@gmail.com>
2023-03-03 14:40:27 +0300

    precommit-11

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ locals {
 
   environment = "stg"
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+            abc         = "def"
+>>>>>>> Stashed changes
+=======
+            abc         = "def"
+>>>>>>> Stashed changes
 =======
             abc         = "def"
 >>>>>>> Stashed changes
